66b1a8bce817c0dba010825a8d3288b28b7cb2c8
gt.park <smileboy0014@gmail.com>
2024-06-23 23:12:12 +0900

    first commit

---
+++ src/main/java/com/hhplus/clean/HhplusCleanJavaWeek02Application.java
@@ -0,0 +1,13 @@
+package com.hhplus.clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusCleanJavaWeek02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusCleanJavaWeek02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/clean/HhplusCleanJavaWeek02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusCleanJavaWeek02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
